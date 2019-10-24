e69c099724e05da0060e81a5db2ab1a43b89779b
Daehan Lee <alice.p.liddell0v0@gmail.com>
2019-10-24 11:39:24 +0900

    ver0.1
    ver0.1

---
+++ Assets/Scripts/Plus.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plus : MonoBehaviour
+{
+    public GameObject plusPanel;
+    [SerializeField]
+    private bool isPanelOn;
+
+    private void Start()
+    {
+        plusPanel.SetActive(false);
+        isPanelOn = false;
+    }
+
+    public void OpenPlusPanel()
+    {
+        if (!isPanelOn)
+        {
+            plusPanel.SetActive(true);
+        }
+        else
+        {
+            plusPanel.SetActive(false);
+        }
+
+        isPanelOn = !isPanelOn;
+    }
+}
